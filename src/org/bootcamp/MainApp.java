e62c8c536f2a3c4158ef0a5329db43cb9961eba5
Carla <carla_niculescu@yahoo.com>
2018-04-18 17:59:20 +0300

    First commit

---
+++ src/org/bootcamp/MainApp.java
@@ -0,0 +1,8 @@
+package org.bootcamp;
+//redenumire clasa - click pe nume clasa, Shift+F6 -> nume nou (modificare automata la fisier)
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
